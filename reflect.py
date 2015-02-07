540895d6ce85b9011a1cb09a009c95b5ec4e1508
arathorn593 <rjosephp95@gmail.com>
2015-02-07 14:30:13 -0500

    added raytrace function

---
+++ reflect.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def find_intersection(incident_ori, incident_dir, obj_list):
+    minDist = None
+    minDistIndex = 0
+
+    for i in xrange(len(obj_list)):
+        dist = obj_list[i].does_intersect(incident_ori, incident_dir)
+
+        if(dist != None and (minDist == None or dist < minDist):
+            minDist = dist
+            minDistIndex = i
+
+    return (minDist, obj_list[minDistIndex])
+
+    
