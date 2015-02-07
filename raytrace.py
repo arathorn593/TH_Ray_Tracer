c6d04be14b20071b6f9b32e74069d768a87bf881
Devin Gund <devingund@gmail.com>
2015-02-07 14:40:51 -0500

    Added conditional reflective and refractive checks

--- raytrace.py
+++ raytrace.py
@@ -23,13 +23,17 @@ def raytrace_main(incident_ori, incident_dir, dist, obj_list, light_list):
         #add matte color
         add_color(pix_color, obj.color, obj.material.matte_scale)
 
-        #add reflected color
-        norm_vect = obj.get_surface_norm(incident_ori, incident_dir, distance)
-        refl_vect = incident_ori - (norm_vect * 2 * (np.dot(incident_ori, norm_vect)))
-        intersect_point = incident_ori + distance * incident_dir
-        refl_color = raytrace_main(intersect_point, refl_vect, dist, obj_list, light_list)
-        if(refl_color != None):
-            add_color(pix_color, refl_color, obj.material.refl_scale)
+        #add reflected color if necessary
+        if obj.is_reflective:
+            norm_vect = obj.get_surface_norm(incident_ori, incident_dir, distance)
+            refl_vect = incident_ori - (norm_vect * 2 * (np.dot(incident_ori, norm_vect)))
+            intersect_point = incident_ori + distance * incident_dir
+            refl_color = raytrace_main(intersect_point, refl_vect, dist, obj_list, light_list)
+            if(refl_color != None):
+                add_color(pix_color, refl_color, obj.material.refl_scale)
+
+        if obj.is_refractive:
+            pass
 
         return pix_color
 
