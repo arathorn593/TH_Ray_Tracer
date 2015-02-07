fc0eca292f167fccef92d4adfd3ecb9e5d0c4dae
Devin Gund <devingund@gmail.com>
2015-02-07 13:47:41 -0500

    Added intersection checking

--- raytrace.py
+++ raytrace.py
@@ -1,11 +1,9 @@
-raytrace_main(incident_ori, incident_dir, distance_travelled, obj_list):
-    gridWidth = 0
-    gridHeight = 0
-
-    for x in xrange(gridWidth):
-        for y in xrange(gridHeight):
-            color = 0
-            ray = (incident_ori, incident_dir)
-            distance = find_intersection(incident_ori, incident_dir)
-            if distance:
+def raytrace_main(incident_ori, incident_dir, distance_travelled, obj_list, light_list):
+        color = 0
+        ray = (incident_ori, incident_dir)
+        (distance, obj) = find_intersection(incident_ori, incident_dir, obj_list)
+        if distance:
+            for i in xrange(len(light_list)):
+                # calculate lighting contribution
                 pass
+            
