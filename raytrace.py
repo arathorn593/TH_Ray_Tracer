7a1529f36f8b2b6646819a31fa84ca64d517148c
Devin Gund <devingund@gmail.com>
2015-02-07 15:08:26 -0500

    Modified pixel storage

--- raytrace.py
+++ raytrace.py
@@ -4,7 +4,7 @@ def add_color(color_main, color_to_add, scale):
     for i in xrange(len(color_main):
         color_main[i] += color_to_add * scale
 
-def raytrace_main(incident_ori, incident_dir, dist, obj_list, light_list):
+def raytrace_main(incident_ori, incident_dir, dist, obj_list, pixel):
     #maximum distance a ray can travel
     maxDist = 100
     if(dist > maxDist):
@@ -15,15 +15,11 @@ def raytrace_main(incident_ori, incident_dir, dist, obj_list, light_list):
 
 
     if distance:
-        pix_color = [0, 0, 0]
         dist += distance
         dist_scale = 1.0/(dist)
-        for i in xrange(len(light_list)):
-            # calculate lighting contribution
-            pass
         
         #add matte color
-        add_color(pix_color, obj.color, obj.material.matte_scale)
+        add_color(pixel, obj.color, obj.material.matte_scale)
 
         #add reflected color if necessary
         if obj.is_reflective:
@@ -32,11 +28,7 @@ def raytrace_main(incident_ori, incident_dir, dist, obj_list, light_list):
             intersect_point = incident_ori + distance * incident_dir
             refl_color = raytrace_main(intersect_point, refl_vect, dist, obj_list, light_list)
             if(refl_color != None):
-                add_color(pix_color, refl_color, obj.material.refl_scale)
+                add_color(pixel, refl_color, obj.material.refl_scale)
 
         if obj.is_refractive:
             pass
-
-        return pix_color
-
-    return None
